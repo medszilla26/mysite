2b59619f0a8dd632d48c7242d60b6d85743df24e
medszilla26 <pablohdzjr@gmail.com>
2020-10-05 00:41:14 -0500

    still need work on footer, stays on top on fullscreen

--- src/layout/footer/index.js
+++ src/layout/footer/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.css";
 import { Col, Row } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import pablologo from "../../assets/branding/pablo-logo.png";
 
 function Footer() {
@@ -8,19 +9,41 @@ function Footer() {
     <footer>
       <div className="wrap">
         <Row>
-          <Col md={3} sm={8}>
+          <Col md={4} sm={8}>
             <img className="logo-footer" src={pablologo} alt="branding logo" />
           </Col>
-          <Col md={3} sm={8}>
-            <h4>Reach Me Here</h4>
+          <Col md={4} sm={8} className="social-list">
+            <ul className="list-unstyled social-icons">
+              <li>
+                <FontAwesomeIcon
+                  icon={["fab", "github"]}
+                  size="3x"
+                  className="ico"
+                />
+              </li>
+              <li>
+                <FontAwesomeIcon
+                  icon={["fab", "linkedin"]}
+                  size="3x"
+                  className="ico"
+                />
+              </li>
+              <li>
+                <FontAwesomeIcon
+                  icon={["fab", "instagram"]}
+                  size="3x"
+                  className="ico"
+                />
+              </li>
+            </ul>
           </Col>
-          <Col md={3} sm={8}>
-            <h4>Reach Me Here</h4>
+          <Col md={3} sm={8} className="location">
+            <h5>Houston, Tx</h5>
           </Col>
         </Row>
         <Row>
           <Col className="footer-bottom">
-            <p className="footer-copyright">
+            <p className="footer-copyright text-muted">
               Copyright &copy; {new Date().getFullYear()} Pablo Hernandez Jr
             </p>
           </Col>
