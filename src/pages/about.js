9b3d65523063199092a245fca2fd16907bb831b6
medszilla26 <pablohdzjr@gmail.com>
2020-10-08 01:11:35 -0500

    started working on ABOUT page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,22 +1,26 @@
 import React, { Component } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 class About extends Component {
   render() {
     return (
       <Container>
-        <h1>This is the About Page</h1>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </p>
+        <Row className="justify-content-lg-center">
+          <Col lg="6">
+            <div className="aboutme">
+              <h1>Pablo Hernandez</h1>
+              <h6>Front End Developer</h6>
+              <p>
+                Morbi quis turpis pharetra, sagittis erat in, finibus eros.
+                Integer ultricies mi ac eros dignissim dapibus. Nulla tortor
+                ante, consectetur at porta ut, aliquam vitae libero. Donec
+                cursus, augue ut facilisis tincidunt, nibh nulla facilisis odio,
+                vitae mattis quam ipsum sed nisi. Orci varius natoque penatibus.
+              </p>
+            </div>
+          </Col>
+          <Col lg="6">Image goes here</Col>
+        </Row>
       </Container>
     );
   }
