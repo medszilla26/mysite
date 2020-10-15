c95f3fc8ee18c61253b160543da7e3101170b715
medszilla26 <pablohdzjr@gmail.com>
2020-10-15 18:53:37 -0500

    Updates

---
+++ precache-manifest.02ce113c07d11c30f513e31ff12f4428.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66ad532a55cc3ae738c5ecd7056bafb4",
+    "url": "/mysite/index.html"
+  },
+  {
+    "revision": "dd0a5875585b8f84da9d",
+    "url": "/mysite/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f673e2e1a29f05cf788f",
+    "url": "/mysite/static/css/main.8273393a.chunk.css"
+  },
+  {
+    "revision": "dd0a5875585b8f84da9d",
+    "url": "/mysite/static/js/2.546e61f8.chunk.js"
+  },
+  {
+    "revision": "6f9c91f92830cd94e0e9d23e81437a49",
+    "url": "/mysite/static/js/2.546e61f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f673e2e1a29f05cf788f",
+    "url": "/mysite/static/js/main.6a087d9b.chunk.js"
+  },
+  {
+    "revision": "1d99fe0da908ca569518",
+    "url": "/mysite/static/js/runtime-main.1d89857a.js"
+  },
+  {
+    "revision": "bb9bbd91b8d9d3d605e246e9eed89f7a",
+    "url": "/mysite/static/media/BrewFinder.bb9bbd91.png"
+  },
+  {
+    "revision": "6ff49fc3de46b690ee62d45cbed3c8ea",
+    "url": "/mysite/static/media/BurgerApp.6ff49fc3.png"
+  },
+  {
+    "revision": "e9f7a6a7f66200b4366141390de33f28",
+    "url": "/mysite/static/media/CodeQuiz.e9f7a6a7.png"
+  },
+  {
+    "revision": "2641a2a0c55fc70f59d6577c7a1f2df2",
+    "url": "/mysite/static/media/PasswordGenerator.2641a2a0.png"
+  },
+  {
+    "revision": "9880cd40ab1441f80a1d2ad0b5b794a9",
+    "url": "/mysite/static/media/pablo-logo.9880cd40.png"
+  },
+  {
+    "revision": "88dd93ce4b0d7bcbd2d69b1141b632cb",
+    "url": "/mysite/static/media/pablohdz.88dd93ce.jpeg"
+  },
+  {
+    "revision": "5b4af97cd84956fc34e6eed0cd15b8b1",
+    "url": "/mysite/static/media/resume.5b4af97c.pdf"
+  }
+]);
