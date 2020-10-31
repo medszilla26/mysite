9ea99351c477a62831af230bebc04f2bdfcef6c6
M3D5 <pablohdzjr@gmail.com>
2020-10-30 22:24:13 -0500

    continued styling sidebar on remake-resume page

--- src/pages/resume2/resume-remake.js
+++ src/pages/resume2/resume-remake.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./style.css";
+import ProgressBar from "../../components/progbar";
 import { Container, Row, Col } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class Resume2 extends Component {
   render() {
@@ -15,6 +17,31 @@ class Resume2 extends Component {
                   (832) 904-1663 &nbsp;|&nbsp; pablohdzjr@gmail.com
                   &nbsp;|&nbsp; Houston, Texas
                 </p>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={{ span: 4, order: 1 }} xs={{ span: 12, order: 2 }}>
+              <div className="sidebar">
+                <ul className="list-unstyled">
+                  <FontAwesomeIcon icon={["fab", "github-alt"]} />
+                  <li>github.com/medszilla26</li>
+                  <FontAwesomeIcon icon={["fab", "linkedin-in"]} />
+                  <li>linkedin.com/in/pablohdzjr</li>
+                </ul>
+              </div>
+              <div className="progress-container">
+                <h6>SKILLS</h6>
+                <ProgressBar name="HTML5" value={80} />
+                <ProgressBar name="CSS3" value={80} />
+                <ProgressBar name="JavaScript" value={80} />
+                <ProgressBar name="React" value={80} />
+                <ProgressBar name="Bootstrap" value={80} />
+                <ProgressBar name="Foundation" value={80} />
+                <ProgressBar name="JQuery" value={80} />
+                <h6>LANGUAGES</h6>
+                <ProgressBar name="English" value={100} />
+                <ProgressBar name="Spanish" value={90} />
               </div>
             </Col>
           </Row>
